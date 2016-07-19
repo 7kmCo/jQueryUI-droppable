4c3890f958f097798cea5d94b37d4b8418a6fb46
Foad Yousefi <foadyousefi@gmail.com>
2016-07-19 14:12:27 +0200

    Solve draggable classes on dynamically added elements

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 
 $( function() {
+  // Draggable element
   $( ".draggable" ).draggable({ revert: true, zIndex: 100 });
+  // Droppable element
   $( "#featured, .teaser" ).droppable({
      hoverClass: 'isHover',
      tolerance: "pointer",
+    // Function for handling Drop event
     drop: function( event, ui ) {
       var draggableId = ui.draggable.attr("id");
       var droppableId = $(this).attr("id");
+      // Reload new droped element into droppable
       $(this).load("droppable.php", {"targetID[]": [draggableId, droppableId] });
-      ui.draggable.remove();
+      // Store changes into database
       $.ajax({
             type: "GET",
             url: "publish.php?id="+draggableId+"&place="+droppableId,
         });
+        // Event after draggable droped
         $("#info").text("Document have been changed.")
-        //setTimeout(function(){$("#draggable").load("draggable.php");}, 100);
-        //location.reload();
+        // After Drop event and storing into database, reload droppable element
+        setTimeout(function(){
+          $("#draggable").load("draggable.php")
+          .on( "mouseover", function() {
+            $( ".draggable" ).draggable({ revert: true, zIndex: 100 });
+          });
+        }, 100);
+
     }
   });
 });
